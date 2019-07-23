2ec12a089233839f8a01a69691f809a80f79339c
duynd@nexlesoft.com <duynd@nexlesoft.com>
2019-07-23 22:26:34 +0700

    Updates

---
+++ precache-manifest.3379f00cea1586f589ad5cbb6dfddfd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f616dd6ae2c4233730cd8ea7ed51281",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d93281dd9638c94db023",
+    "url": "/static/css/main.d2d92cc0.chunk.css"
+  },
+  {
+    "revision": "241747477c43dd162d5c",
+    "url": "/static/js/2.24774a61.chunk.js"
+  },
+  {
+    "revision": "d93281dd9638c94db023",
+    "url": "/static/js/main.29ce956e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nasa-collection/precache-manifest.c9e4c8c83c1d2deaf3397aab8c0f223f.js"
+  "/precache-manifest.3379f00cea1586f589ad5cbb6dfddfd1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nasa-collection/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
